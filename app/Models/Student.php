755eb63947413b6a6319baf1c542d8de7ee3c227
Shishir Adhikari <shishiradhikari1829@gmail.com>
2026-07-02 18:46:02 +0545

    create a model and defile one to one relationship

---
+++ app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+    ];
+
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class, 'student_id', 'id');
+    }
+
+}
